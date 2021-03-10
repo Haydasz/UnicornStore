55cd47510e251238d06e66380a956e0f82757285
Katarzyna Haydasz <k.haydasz@gmail.com>
2021-03-10 10:10:26 +0100

    .

---
+++ UnicornStore/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using UnicornStore.Models;
+
+namespace UnicornStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Login(LoginModel loginModel)
+        {
+            var user = new User() { Name = "test", Id = 1 , Role = "Admin", test = "red blue hahah"}; //TODO find user
+
+            if (user == null)
+                return Unauthorized();
+
+            var claims = new List<Claim> {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Name),
+                new Claim(ClaimTypes.Role, user.Role),
+                new Claim("test", user.test)
+
+            };
+
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var principal = new ClaimsPrincipal(identity);
+
+            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, new AuthenticationProperties { IsPersistent = loginModel.Remember });
+
+            return View("Index");
+        }
+
+        public IActionResult Logout()
+        {
+             HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return Redirect("/");
+        }
+    }
+}
---
+++ UnicornStore/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace UnicornStore.Controllers
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string test { get; set; }
+        public string Role { get; set; }
+        
+    }
+}
